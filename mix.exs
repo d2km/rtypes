b4d716c50b72348152cba214687cfc713eae4780
Denis Korzunov <disya2@gmail.com>
2019-05-26 20:54:15 +1000

    Bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RTypes.MixProject do
   def project do
     [
       app: :rtypes,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: "Automatically generate run time type checkers",
