c2a3d61645a89e2333d8cbeb6b56e51187280891
Denis Korzunov <disya2@gmail.com>
2019-12-20 14:28:59 +1100

    Move expand_type_args and decompose_and_expand to Internal module

--- lib/rtypes.ex
+++ lib/rtypes.ex
@@ -61,6 +61,8 @@ defmodule RTypes do
 
   """
 
+  import RTypes.Internal, only: [decompose_and_expand: 2, expand_type_args: 1]
+
   @typedoc "`t:error_decription/1` is a keyword list which details the validation error."
   @type error_description :: [
           {:message, String.t()}
@@ -125,28 +127,6 @@ defmodule RTypes do
       fn term ->
         RTypes.Checker.check(term, typ)
       end
-    end
-  end
-
-  defp expand_type_args(args) do
-    Enum.map(args, fn arg ->
-      case arg do
-        {mod, type_name, type_args} ->
-          {:remote_type, 0, [{:atom, 0, mod}, {:atom, 0, type_name}, expand_type_args(type_args)]}
-
-        {type_name, type_args} ->
-          {:type, 0, type_name, expand_type_args(type_args)}
-      end
-    end)
-  end
-
-  defp decompose_and_expand(expr, env) do
-    case Macro.decompose_call(expr) do
-      {mod, f, args} ->
-        {Macro.expand(mod, env), f, Enum.map(args, &decompose_and_expand(&1, env))}
-
-      {f, args} ->
-        {f, Enum.map(args, &decompose_and_expand(&1, env))}
     end
   end
 
